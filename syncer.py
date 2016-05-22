618e7fafb4d0a809c6d84791aa2b67a2fd003315
Bartek Jarocki <bartek@smatly.com>
2016-05-22 14:25:55 +0200

    [rsync] add --temp-dir

--- syncer.py
+++ syncer.py
@@ -14,7 +14,11 @@ for data in cursor:
     if not sf['synced_nodes'].get(socket.gethostname()):
         print('Need to sync this guy!', sf)
         source_node = sf.get('source_node')
-        failed = subprocess.call(['rsync', '-Ra', '{}::storage{}'.format(source_node, path), '/'])
+        failed = subprocess.call([
+            'rsync', '--temp-dir', '/tmp',
+            '-Ra', '{}::storage{}'.format(source_node, path),
+            '/'
+        ])
         if not failed:
             StorageDB.update(path, {'synced_nodes': {socket.gethostname(): True}})
         else:
